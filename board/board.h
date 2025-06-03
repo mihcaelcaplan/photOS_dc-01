a8fa6383b6e066c76cdb6dc8b5329a69a902d67a
mihcaelcaplan <caplan.j.michael@gmail.com>
2025-06-03 13:28:29 +0200

    adding sd driver support

--- board/board.h
+++ board/board.h
@@ -33,25 +33,92 @@
 #define BOARD_DEBUG_UART_BAUDRATE (115200U)
 #endif /* BOARD_DEBUG_UART_BAUDRATE */
 
+/*! @brief The USER_LED used for board */
+#define LOGIC_LED_ON  (0U)
+#define LOGIC_LED_OFF (1U)
+#ifndef BOARD_USER_LED_GPIO
+#define BOARD_USER_LED_GPIO GPIO1
+#endif
+#ifndef BOARD_USER_LED_GPIO_PIN
+#define BOARD_USER_LED_GPIO_PIN (9U)
+#endif
+
+#define USER_LED_INIT(output)                                            \
+    GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, output); \
+    BOARD_USER_LED_GPIO->GDIR |= (1U << BOARD_USER_LED_GPIO_PIN) /*!< Enable target USER_LED */
+#define USER_LED_ON() \
+    GPIO_PortClear(BOARD_USER_LED_GPIO, 1U << BOARD_USER_LED_GPIO_PIN)                  /*!< Turn off target USER_LED */
+#define USER_LED_OFF() GPIO_PortSet(BOARD_USER_LED_GPIO, 1U << BOARD_USER_LED_GPIO_PIN) /*!<Turn on target USER_LED*/
+#define USER_LED_TOGGLE()                                       \
+    GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, \
+                  0x1 ^ GPIO_PinRead(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN)) /*!< Toggle target USER_LED */
 
 /*! @brief Define the port interrupt number for the board switches */
-//#ifndef BOARD_USER_BUTTON_GPIO
-//#define BOARD_USER_BUTTON_GPIO GPIO5
-//#endif
-//#ifndef BOARD_USER_BUTTON_GPIO_PIN
-//#define BOARD_USER_BUTTON_GPIO_PIN (0U)
-//#endif
-//#define BOARD_USER_BUTTON_IRQ         GPIO5_Combined_0_15_IRQn
-//#define BOARD_USER_BUTTON_IRQ_HANDLER GPIO5_Combined_0_15_IRQHandler
-//#define BOARD_USER_BUTTON_NAME        "SW8"
+#ifndef BOARD_USER_BUTTON_GPIO
+#define BOARD_USER_BUTTON_GPIO GPIO5
+#endif
+#ifndef BOARD_USER_BUTTON_GPIO_PIN
+#define BOARD_USER_BUTTON_GPIO_PIN (0U)
+#endif
+#define BOARD_USER_BUTTON_IRQ         GPIO5_Combined_0_15_IRQn
+#define BOARD_USER_BUTTON_IRQ_HANDLER GPIO5_Combined_0_15_IRQHandler
+#define BOARD_USER_BUTTON_NAME        "SW8"
 
 /*! @brief The board flash size */
 #define BOARD_FLASH_SIZE (0x800000U)
 
+/*! @brief The ENET PHY address. */
+#define BOARD_ENET0_PHY_ADDRESS (0x02U) /* Phy address of enet port 0. */
+
 /* USB PHY condfiguration */
-//#define BOARD_USB_PHY_D_CAL     (0x0CU)
-//#define BOARD_USB_PHY_TXCAL45DP (0x06U)
-//#define BOARD_USB_PHY_TXCAL45DM (0x06U)
+#define BOARD_USB_PHY_D_CAL     (0x0CU)
+#define BOARD_USB_PHY_TXCAL45DP (0x06U)
+#define BOARD_USB_PHY_TXCAL45DM (0x06U)
+
+#define BOARD_ARDUINO_INT_IRQ   (GPIO1_INT3_IRQn)
+#define BOARD_ARDUINO_I2C_IRQ   (LPI2C1_IRQn)
+#define BOARD_ARDUINO_I2C_INDEX (1)
+
+/*! @brief The WIFI-QCA shield pin. */
+#define BOARD_INITGT202SHIELD_PWRON_GPIO      GPIO1               /*!< GPIO device name: GPIO */
+#define BOARD_INITGT202SHIELD_PWRON_PORT      1U                  /*!< PORT device index: 1 */
+#define BOARD_INITGT202SHIELD_PWRON_GPIO_PIN  3U                  /*!< PIO4 pin index: 3 */
+#define BOARD_INITGT202SHIELD_PWRON_PIN_NAME  GPIO1_3             /*!< Pin name */
+#define BOARD_INITGT202SHIELD_PWRON_LABEL     "PWRON"             /*!< Label */
+#define BOARD_INITGT202SHIELD_PWRON_NAME      "PWRON"             /*!< Identifier name */
+#define BOARD_INITGT202SHIELD_PWRON_DIRECTION kGPIO_DigitalOutput /*!< Direction */
+
+#define BOARD_INITGT202SHIELD_IRQ_GPIO      GPIO1              /*!< GPIO device name: GPIO */
+#define BOARD_INITGT202SHIELD_IRQ_PORT      1U                 /*!< PORT device index: 1 */
+#define BOARD_INITGT202SHIELD_IRQ_GPIO_PIN  19U                /*!< PIO1 pin index: 19 */
+#define BOARD_INITGT202SHIELD_IRQ_PIN_NAME  GPIO1_19           /*!< Pin name */
+#define BOARD_INITGT202SHIELD_IRQ_LABEL     "IRQ"              /*!< Label */
+#define BOARD_INITGT202SHIELD_IRQ_NAME      "IRQ"              /*!< Identifier name */
+#define BOARD_INITGT202SHIELD_IRQ_DIRECTION kGPIO_DigitalInput /*!< Direction */
+
+#define BOARD_INITSILEX2401SHIELD_PWRON_GPIO      GPIO1               /*!< GPIO device name: GPIO */
+#define BOARD_INITSILEX2401SHIELD_PWRON_PORT      1U                  /*!< PORT device index: 1 */
+#define BOARD_INITSILEX2401SHIELD_PWRON_GPIO_PIN  9U                  /*!< PIO4 pin index: 9 */
+#define BOARD_INITSILEX2401SHIELD_PWRON_PIN_NAME  GPIO1_9             /*!< Pin name */
+#define BOARD_INITSILEX2401SHIELD_PWRON_LABEL     "PWRON"             /*!< Label */
+#define BOARD_INITSILEX2401SHIELD_PWRON_NAME      "PWRON"             /*!< Identifier name */
+#define BOARD_INITSILEX2401SHIELD_PWRON_DIRECTION kGPIO_DigitalOutput /*!< Direction */
+
+#define BOARD_INITSILEX2401SHIELD_IRQ_GPIO      GPIO1              /*!< GPIO device name: GPIO */
+#define BOARD_INITSILEX2401SHIELD_IRQ_PORT      1U                 /*!< PORT device index: 1 */
+#define BOARD_INITSILEX2401SHIELD_IRQ_GPIO_PIN  11U                /*!< PIO1 pin index: 11 */
+#define BOARD_INITSILEX2401SHIELD_IRQ_PIN_NAME  GPIO1_11           /*!< Pin name */
+#define BOARD_INITSILEX2401SHIELD_IRQ_LABEL     "IRQ"              /*!< Label */
+#define BOARD_INITSILEX2401SHIELD_IRQ_NAME      "IRQ"              /*!< Identifier name */
+#define BOARD_INITSILEX2401SHIELD_IRQ_DIRECTION kGPIO_DigitalInput /*!< Direction */
+
+/* @Brief Board accelerator sensor configuration */
+#define BOARD_ACCEL_I2C_BASEADDR LPI2C1
+/* Select USB1 PLL (480 MHz) as LPI2C's clock source */
+#define BOARD_ACCEL_I2C_CLOCK_SOURCE_SELECT (0U)
+/* Clock divider for LPI2C clock source */
+#define BOARD_ACCEL_I2C_CLOCK_SOURCE_DIVIDER (5U)
+#define BOARD_ACCEL_I2C_CLOCK_FREQ           (CLOCK_GetFreq(kCLOCK_Usb1PllClk) / 8 / (BOARD_ACCEL_I2C_CLOCK_SOURCE_DIVIDER + 1U))
 
 #define BOARD_CODEC_I2C_BASEADDR             LPI2C1
 #define BOARD_CODEC_I2C_INSTANCE             1U
@@ -60,21 +127,29 @@
 #define BOARD_CODEC_I2C_CLOCK_FREQ           (10000000U)
 
 /* @Brief Board CAMERA configuration */
-//#define BOARD_CAMERA_I2C_BASEADDR             LPI2C1
-//#define BOARD_CAMERA_I2C_CLOCK_SOURCE_DIVIDER (5U)
-//#define BOARD_CAMERA_I2C_CLOCK_SOURCE_SELECT  (0U) /* Select USB1 PLL (480 MHz) as LPI2C's clock source */
-//#define BOARD_CAMERA_I2C_CLOCK_FREQ \
-//    (CLOCK_GetFreq(kCLOCK_Usb1PllClk) / 8 / (BOARD_CAMERA_I2C_CLOCK_SOURCE_DIVIDER + 1U))
-//
-//#define BOARD_CAMERA_I2C_SCL_GPIO GPIO1
-//#define BOARD_CAMERA_I2C_SCL_PIN  16
-//#define BOARD_CAMERA_I2C_SDA_GPIO GPIO1
-//#define BOARD_CAMERA_I2C_SDA_PIN  17
-//#define BOARD_CAMERA_PWDN_GPIO    GPIO1
-//#define BOARD_CAMERA_PWDN_PIN     4
+#define BOARD_CAMERA_I2C_BASEADDR             LPI2C1
+#define BOARD_CAMERA_I2C_CLOCK_SOURCE_DIVIDER (5U)
+#define BOARD_CAMERA_I2C_CLOCK_SOURCE_SELECT  (0U) /* Select USB1 PLL (480 MHz) as LPI2C's clock source */
+#define BOARD_CAMERA_I2C_CLOCK_FREQ \
+    (CLOCK_GetFreq(kCLOCK_Usb1PllClk) / 8 / (BOARD_CAMERA_I2C_CLOCK_SOURCE_DIVIDER + 1U))
+
+#define BOARD_CAMERA_I2C_SCL_GPIO GPIO1
+#define BOARD_CAMERA_I2C_SCL_PIN  16
+#define BOARD_CAMERA_I2C_SDA_GPIO GPIO1
+#define BOARD_CAMERA_I2C_SDA_PIN  17
+#define BOARD_CAMERA_PWDN_GPIO    GPIO1
+#define BOARD_CAMERA_PWDN_PIN     4
+
+/* @Brief Board Bluetooth HCI UART configuration */
+#define BOARD_BT_UART_BASEADDR    LPUART3
+#define BOARD_BT_UART_INSTANCE    3
+#define BOARD_BT_UART_BAUDRATE    3000000
+#define BOARD_BT_UART_CLK_FREQ    BOARD_DebugConsoleSrcFreq()
+#define BOARD_BT_UART_IRQ         LPUART3_IRQn
+#define BOARD_BT_UART_IRQ_HANDLER LPUART3_IRQHandler
 
 /*! @brief board has sdcard */
-//#define BOARD_HAS_SDCARD (1U)
+#define BOARD_HAS_SDCARD (1U)
 
 #if defined(__cplusplus)
 extern "C" {
