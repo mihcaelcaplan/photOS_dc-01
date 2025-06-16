cc33263ae7979ea501d4bd5ea5757dac16cafad3
mihcaelcaplan <caplan.j.michael@gmail.com>
2025-06-16 16:11:45 +0200

    adding sdk support for jpeg and file system for use in a image browser

---
+++ source/display_file.c
@@ -0,0 +1,12 @@
+/*
+ * display_file.c
+ *
+ *  Created on: Jun 16, 2025
+ *      Author: mcaplan
+ */
+
+
+#include "display_file.h"
+
+
+//need to decode a jpeg, set the framebuf and screen on
---
+++ source/display_file.h
@@ -0,0 +1,21 @@
+/*
+ * display_file.h
+ *
+ *  Created on: Jun 16, 2025
+ *      Author: mcaplan
+ */
+
+#ifndef DISPLAY_FILE_H_
+#define DISPLAY_FILE_H_
+
+//
+//DISPLAY_showStoredFile();
+//
+//void getStoredFile(char* filename);
+//
+//void decodeFile(FIL file);
+
+
+
+
+#endif /* DISPLAY_FILE_H_ */
